8e086869c14a6ce7e7454c493c1869e6ed64954e
Jamarr <jamarredwards@gmail.com>
2014-10-26 23:22:46 -0700

    House of Mantra website
    A jewelry webstore

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php # config.inc.php
+
+define('STRIPE_PRIVATE_KEY', 'sk_test_LPde3f8chJD9aLxcGaNRg99E');
+define('STRIPE_PUBLIC_KEY', 'pk_test_b0BssYEUPnqlega4HBQVse7M');
---
+++ store.php
@@ -0,0 +1,68 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="House of Mantra is a brand and culture 
+focused on the mantras the rose recites to surpass each thorn 
+of life to achieve his or her essence of beauty. My most loved is Meditate, Medicate, Mindfulness." />
+    <meta name="author" content="Jamarr Edwards" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+    <title>House of Mantra</title>
+
+    <!--[if lt IE 9]><script src=http://html5shiv.googlecode.com/svn/trunk/html5.js></script><![endif]-->
+
+    <!-- css for H.O.M -->
+    <link href="/assets/css/styles2.css" rel='stylesheet' />
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Fonts for H.O.M -->
+    <link href='//fonts.googleapis.com/css?family=Bad+Script' rel='stylesheet' type='text/css'>
+
+   
+<div class="mainContainer">
+<div class="homePage">
+        <img src="/assets/images/logo.png">
+        <div class="homeNav">
+        <ul>
+            <li><a href="https://store5863081.ecwid.com"><img src="/assets/images/chakra_button.png" alt="Collection" border="0"></a></li>
+            <li><a href="about.html"><img src="/assets/images/about_button.png" alt="About" border="0"></a></li>
+       <script type="text/javascript" src="/assets/js/jquery-1.4.2.min.js"></script>
+
+<script type="text/javascript">
+jQuery(document).ready(function($) {                                                                                                
+    if ($('.pressPage').css("opacity")!= "1") {                             
+    $('.pressPage').fadeTo(500, 1);                             
+    }   
+
+    $('.pageScroll').click(function() {                             
+    if ($('.aboutPage2').css("opacity")!= "1") {                            
+    $('.aboutPage2').fadeTo(500, 1);    
+    $('.aboutPage1').fadeTo(500, 0);                                
+    } else { 
+    $('.aboutPage2').fadeTo(500, 0);    
+    $('.aboutPage1').fadeTo(500, 1); 
+    }
+    });                             
+}   );
+</script>
+</head>
+<body class="body">      <li><a href="shipping.html"><img src="/assets/images/shipping.png" alt="shipping" border="0"></a></li>
+            <li><a href="contact.html"><img src="/assets/images/contact_button.png" alt="contact" border="0"></a></li>  
+                 
+            </ul>   
+        </div>
+       
+</div><!-- end of HomePage -->
+    <div class="aboutPage1">
+  <div id="my-store-5863081"></div>
+<div>
+<script type="text/javascript" src="//app.ecwid.com/script.js?5863081" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(1,3) list(10) table(20)","categoryView=grid","searchView=list","id=my-store-5863081");</script>
+</div>   
+</div><!--end pressPage"-->
+</div>
